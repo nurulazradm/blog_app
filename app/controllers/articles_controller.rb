9420bd45c956627443188fad8e0944ded7e9af46
nurulazradm <nurulazrad@gmail.com>
2018-05-20 23:07:18 +0800

    Complete new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,16 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path, notice: 'Article has been created'
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
