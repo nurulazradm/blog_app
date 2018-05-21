0c354294c59a62841305d14dabb7d4f052839c17
nurulazradm <nurulazrad@gmail.com>
2018-05-22 00:07:58 +0800

    Update spec to verify owner sees edit and delete buttons

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -35,4 +35,17 @@ RSpec.feature "Showing an article", type: :feature do
     expect(page).not_to have_link 'Edit Article'
     expect(page).not_to have_link 'Delete Article'
   end
+
+  scenario "A signed in owner sees both the Edit and Delete buttons" do
+    login_as @user
+    visit root_path
+
+    click_link @article.title
+
+    expect(page).to have_content @article.title
+    expect(page).to have_content @article.body
+    expect(current_path).to eq(article_path(@article))
+    expect(page).to have_link 'Edit Article'
+    expect(page).to have_link 'Delete Article'
+  end
 end
