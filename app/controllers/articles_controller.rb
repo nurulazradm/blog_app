0293ee3af92105bc44c1f9d3aedfbbe4609613d4
Nurulazrad M <nurulazrad@gmail.com>
2018-05-22 09:38:52 +0800

    Controller level access restriction

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,14 +25,23 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    unless @article.user == current_user
+      flash[:alert] = "You can only edit your own article."
+      redirect_to root_path
+    end
   end
 
   def update
-    if @article.update(article_params)
-      redirect_to @article, notice: 'Article has been updated'
+    unless @article.user == current_user
+      flash[:danger] = "You can only edit your own article."
+      redirect_to root_path
     else
-      flash.now[:danger] = 'Article has not been updated'
-      render :edit
+      if @article.update(article_params)
+        redirect_to @article, notice: 'Article has been updated'
+      else
+        flash.now[:danger] = 'Article has not been updated'
+        render :edit
+      end
     end
   end
 
--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
   before do
-    @article = Article.create(title: 'Title One', body: 'Body of article title one')
+    @user = User.create(email: 'user@example.com', password: 'secret')
+    @friend = User.create(email: 'friend@example.com', password: 'secret')
+    @article = Article.create!(
+      title: 'Title One',
+      body: 'Body of article title one',
+      user: @user
+    )
   end
 
   describe "GET /articles/:id" do
@@ -22,6 +28,42 @@ RSpec.describe "Articles", type: :request do
         expect(response).to have_http_status(302)
         flash_message = "The article you are looking for could not be found"
         expect(flash[:alert]).to eq flash_message
+      end
+    end
+  end
+
+  describe "Get /articles/:id/edit" do
+    context "with non-signed in user" do
+      before { get "/articles/#{@article.id}/edit" }
+
+      it "redirects to the signin page" do
+        expect(response).to have_http_status(302)
+        flash_message = "You need to sign in or sign up before continuing."
+        expect(flash[:alert]).to eq(flash_message)
+      end
+    end
+
+    context "with signed in user who is non owner" do
+      before do
+        login_as @friend
+        get "/articles/#{@article.id}/edit"
+      end
+
+      it "redirects to the home page" do
+        expect(response).to have_http_status(302)
+        flash_message = "You can only edit your own article."
+        expect(flash[:alert]).to eq(flash_message)
+      end
+    end
+
+    context "with signed in user as owner successful edit" do
+      before do
+        login_as @user
+        get "/articles/#{@article.id}/edit"
+      end
+
+      it "successfully edits article" do
+        expect(response).to have_http_status(200)
       end
     end
   end
